2e54a66f547d98616248ca6a9e56e79871943997
Vijay Navnath Kamble <vkambl@codeaurora.org>
2017-02-01 12:22:50 +0530

    meta-qti-bsp: Mount /cache partition for apq8017-qsap.
    cache partition is required for OTA Upgrade. It holds
    the update.zip package required for OTA Upgrade.
    
    Change-Id: I3749f6c118bcb585feccf344387538bafbc206b0

--- recipes-bsp/startup-scripts/files/apq8017/find_partitions.sh
+++ recipes-bsp/startup-scripts/files/apq8017/find_partitions.sh
@@ -40,6 +40,7 @@ FindAndMountEXT4 () {
 FindAndMountEXT4 userdata /data
 FindAndMountEXT4 persist /persist
 FindAndMountEXT4 dsp /dsp
+FindAndMountEXT4 cache  /cache
 /sbin/restorecon -RF  /dev
 /sbin/restorecon -RF /data/misc/wifi
 exit 0
